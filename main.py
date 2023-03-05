1b82d44c17549d834c42e1699a04474ca772b616
Vikramaditya Chhapwale <magneta@PuddnChef.local>
2023-03-05 13:27:40 +0000

    Init push|

---
+++ main.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from enum import Enum
+import random
+
+# CONFIG ENUM 
+class Genre(Enum):
+    Art =  3
+    Stills = 1 
+    Programming = 1 
+
+df = pd.read_csv("kata.csv")
+unique_genres = df["Genre"].unique()
+
+for genre in unique_genres:
+    selected_pd = (df.loc[df['Genre'] == genre])
+    print("\n \nTask for ", genre)
+
+    for value in (selected_pd.sample(Genre[genre].value).values):
+        print (value[0])
+        if ((value[3]) == "Collection"):
+            print("Pick number: ", random.randint(1, value[4]))
